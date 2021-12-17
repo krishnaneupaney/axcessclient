444d4996813cc85408c456f2cade69fe385c10c1
Krishna <krishna@Krishnas-MBP.attlocal.net>
2021-12-16 20:50:17 -0500

    some fixes

--- src/components/InfoSection/InfoSection.elements.js
+++ src/components/InfoSection/InfoSection.elements.js
@@ -77,5 +77,5 @@ export const Subtitle = styled.p`
   margin-bottom: 35px;
   font-size: 18px;
   line-height: 24px;
-  color: black;
+  color: ${({ lightTextDesc }) => (lightTextDesc ? '#a9b3c1' : '#1c2237')};
 `;
