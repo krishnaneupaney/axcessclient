c949489b2e04b9210185620727e956ab9498f638
Krishna <krishna@Krishnas-MBP.attlocal.net>
2021-12-16 20:42:14 -0500

    some fixes

--- src/components/InfoSection/InfoSection.elements.js
+++ src/components/InfoSection/InfoSection.elements.js
@@ -77,5 +77,5 @@ export const Subtitle = styled.p`
   margin-bottom: 35px;
   font-size: 18px;
   line-height: 24px;
-  color: ${({ lightTextDesc }) => (lightTextDesc ? 'dark' : 'dark')};
+  color: black;
 `;
