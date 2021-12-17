9b83f6a0a37f9ae2a7111d683bb96c2a1fe41bb9
Krishna <krishna@Krishnas-MBP.attlocal.net>
2021-12-16 20:38:42 -0500

    some fixes commited

--- src/components/InfoSection/InfoSection.elements.js
+++ src/components/InfoSection/InfoSection.elements.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const InfoSec = styled.div`
   color: #fff;
   padding: 160px 0;
-  background: ${({ lightBg }) => (lightBg ? '#fff' : '#1B1811')};
+  background: ${({ lightBg }) => (lightBg ? '#fff' : '#101522')};
 `;
 
 export const InfoRow = styled.div`
@@ -47,7 +47,7 @@ export const ImgWrapper = styled.div`
 `;
 
 export const TopLine = styled.div`
-  color: ${({ lightTopLine }) => (lightTopLine ? '#FFEBCD' : '#FFEBCD')};
+  color: ${({ lightTopLine }) => (lightTopLine ? '#a9b3c1' : '#4B59F7')};
   font-size: 18px;
   line-height: 16px;
   font-weight: 700;
@@ -69,7 +69,7 @@ export const Heading = styled.h1`
   font-size: 48px;
   line-height: 1.1;
   font-weight: 600;
-  color: ${({ lightText }) => (lightText ? '#fff' : '#1c2237')};
+  color: ${({ lightText }) => (lightText ? '#f7f8fa' : '#1c2237')};
 `;
 
 export const Subtitle = styled.p`
@@ -77,5 +77,5 @@ export const Subtitle = styled.p`
   margin-bottom: 35px;
   font-size: 18px;
   line-height: 24px;
-  color: ${({ lightTextDesc }) => (lightTextDesc ? '#fff' : '#fff')};
+  color: ${({ lightTextDesc }) => (lightTextDesc ? 'dark' : 'dark')};
 `;
