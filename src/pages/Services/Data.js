e64131f44e2f037201781b84a35b9b4faa9f8e0d
Krishna <krishna@Krishnas-MBP.attlocal.net>
2021-12-18 14:40:44 -0500

    some pics added

--- src/pages/Services/Data.js
+++ src/pages/Services/Data.js
@@ -26,17 +26,32 @@ export const homeObjTwo = {
   alt: 'Vault'
 };
 
+
 export const homeObjSeven = {
   lightBg: true,
   lightText: false,
   lightTextDesc: false,
-  topLine: 'Our Missions',
-  headline: 'is what moves us FORWARD',
+  topLine: 'This is Axcess Fund',
+  headline: 'Our mission moves us FORWARD - to deliver genuine financial products that improves lives',
   description:
-    "Our mission at AxcessFund has been to deliver genuine financial products that improve lives of people we serve! That mission will never change! We want to keep every families together!",
+    "Why? Because home matters. Home is where families thrive. But money is also a fundamental ingredient of life, and unlesss you're one of the few percent of Americans with significant wealth, the ability to borrow affects you everyday. Throughout history, affordable lending has been central to unlocking mobility and opportunity - especially for thoise in need!",
   buttonLabel: 'Get Started',
-  imgStart: 'start',
+  imgStart: '',
   img: require('../../images/family.png'),
+  alt: 'Vault'
+};
+
+export const homeObjTwelve = {
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: 'Artificial Learning ',
+  headline: 'AI & ML is upending existing industry',
+  description:
+    "and creating a new ones. Whether self-driving cars, 3D homes, home assistants, or language live-translation, software/machine that learns and improves on its own has moved from research labs to the mainstream in last few years.",
+  buttonLabel: 'Get Started',
+  imgStart: '',
+  img: require('../../images/fast.webp'),
   alt: 'Vault'
 };
 export const homeObjTen = {
@@ -56,15 +71,17 @@ export const homeObjEleven = {
   lightBg: true,
   lightText: false,
   lightTextDesc: false,
-  topLine: 'Choices are not always clear but at Axcess Fund, ',
+  topLine: 'Other lenders are not always clear but at Axcess Fund, ',
   headline: 'The choice is clear! We need to solve this issue together!',
   description:
-    "Late fees? Hidden charges? Sneaky fine print? No thanks, we’ll leave those to the credit card companies. At Axcess Fund, we like to keep it real—and that means no fees, no gotchas, and no regrets.",
+    "Late fees? Hidden charges? Sneaky fine print? No thanks, we’ll leave those to the credit card companies. At Axcess Fund, we like to keep it real—and that means no fees, no gotchas, and no regrets. We're here to solve this by leveraging technology while keeping families together!",
   buttonLabel: 'Get prequalified',
   imgStart: 'start',
   img: require('../../images/stop.webp'),
   alt: 'Vault'
 };
+
+ 
 export const homeObjFour = {
   lightBg: false,
   lightText: true,
--- src/pages/SignUp/About.js
+++ src/pages/SignUp/About.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import {homeObjSeven, homeObjEleven} from '../Services/Data';
+import {homeObjSeven, homeObjEleven, homeObjTwelve} from '../Services/Data';
 import { InfoSection } from '../../components';
-
+import '../SignUp/About.css'
 
 const About = () => {
     return (
@@ -14,6 +14,36 @@ const About = () => {
                 We believe everyone should have Axcess to safe home filled with love and joy!</h2>
         </div>
         <InfoSection {...homeObjSeven} />
+        <h2>Axcess Fund is a leading artificial intelligence (AI) & ML powered lending platform designerd to improve the 
+            Axcess to affordable fund borrowing while reducing the risk and costs of lender for out lending partner. 
+            By leveraging AxcessFund's AI platform, Axcess-powered lenders can offer high approval rates and with lowest APR 
+            while experience lower loss rates by simultineously delivering the exceptional all-digital lending experience to their customers in need! </h2>
+
+        <br />
+        <br />
+        <br />
+        <br />
+        <h2>
+            
+            Lending is centuries old, but has changed little over the decades.
+            <br />
+            <br />
+            <h3>So why us?</h3>
+            <h4>Traditional lenders use simple FICO-based model to decide who is approved and for what interest rate. While simple and intuitive, these "scoreboard" methods are limited in their ability to quantify reasonable risk.</h4>
+
+        </h2>
+        <br />
+        <br />
+        <br />
+        <h1>Four in five Americans</h1>
+        <br />
+        <h2>have never defaulted on a credit product, yet less than half have access to prime credit</h2>
+        <br />
+        <h3>The implication is eye-opening. With a smarter credit modeling focusing on real-data, 
+            <br />
+            lenders could approve almost twicew as many borrowers, with fewer defaults
+        </h3>
+        <InfoSection {...homeObjTwelve} />
         <InfoSection {...homeObjEleven} />
         </>
     )
